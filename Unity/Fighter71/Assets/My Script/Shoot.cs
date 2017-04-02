2cf7efc4b5dc2c8ea1723a13b068fe7df779b4bc
Imran <imufun1@gmail.com>
2017-04-03 01:35:07 +0600

    add

---
+++ Unity/Fighter71/Assets/My Script/Shoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	private float FireRate = 3.5f;
+	private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkShoot ();
+	}
+
+	void checkShoot(){
+
+		// One method input
+
+		//if (Input.GetKey(KeyCode.Mouse0) && Time.time> FireRate) {
+		//	nextFire = Time.time + FireRate;
+		//	Debug.Log ("pressed" + "fire " + nextFire);
+		//} else {
+		//	Debug.Log ("Not pressed");
+		//}
+
+		// other method input
+
+		if (Input.GetButton("Fire1") && Time.time> FireRate) {
+			nextFire = Time.time + FireRate;
+			Debug.Log ("pressed" + "fire " + nextFire);
+		} else {
+			Debug.Log ("Not pressed");
+		}
+	}
+}
---
+++ Unity/Fighter71/Assets/My Script/Welcome.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Chapter1
+{
+	public class Welcome : MonoBehaviour
+	{
+
+		public string myMessage = "Welcome";
+		private Text textWelcome;
+		public GameObject canvasWelcome;
+		public float waitTime = 3.5f;
+
+		// Use this for initialization
+		void Start ()
+		{
+			setInitialMessage ();
+			MyWelcomeMessage ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+
+		void  setInitialMessage ()
+		{
+		
+			textWelcome = GameObject.Find ("TextWelcome").GetComponent<Text> ();
+		}
+
+		void MyWelcomeMessage ()
+		{
+			if (textWelcome != null) {
+				textWelcome.text = myMessage;
+			} else {
+				Debug.LogWarning ("welcome Text not assigned.");
+			}
+			StartCoroutine(DisableCanvas(3.5f));
+		}
+
+		IEnumerator DisableCanvas(float waitTime){
+			yield return new WaitForSeconds (waitTime);
+			canvasWelcome.SetActive (false);
+		}
+	}
+}
