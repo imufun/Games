48f840040b0319d1e2ffe49b5d72fc6060599217
Imran <imufun1@gmail.com>
2017-04-08 06:54:18 +0600

    g

---
+++ Unity/Fighter71/Assets/My Script/Detaction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Chapter1
+{
+	public class Detaction : MonoBehaviour
+	{
+
+		private RaycastHit hit;
+		private LayerMask detectionLayer;
+		private float checkRate = 0.5f;
+		private float range = 5;
+		private float nextCheck;
+		private Transform myTranform;
+
+		// Use this for initialization
+		void Start ()
+		{
+			setInitailReference ();
+		}
+
+		void setInitailReference ()
+		{
+			myTranform = transform;
+			detectionLayer = 1 << 9;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			DetectItem ();
+				
+		}
+
+		void DetectItem ()
+		{
+		
+			if (Time.time > nextCheck) {
+				nextCheck = Time.time + checkRate;
+				if (Physics.Raycast (myTranform.position, myTranform.forward,	out hit, range, detectionLayer)) {
+					Debug.Log (hit.transform.name + "is an item");	
+				}
+			}
+		}
+	}
+}
