62686b0c31386ff8d979449c4f9d6e786d157c33
Imran <imufun1@gmail.com>
2017-05-15 00:59:51 +0600

    Player move with camera

---
+++ Unity/Robot/Assets/Script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+
+	Vector3 velocity  = Vector3.zero;
+	 
+	public float smothTime = .15f;
+
+	void FixedUpdate (){
+		
+		// target postion
+		Vector3 targetPos = target.position;
+
+		//align the camra add the target z position
+		targetPos.z = transform.position.z;
+
+		transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, smothTime);
+
+	}
+}
